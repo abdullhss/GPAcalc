75558b9066588993409666d0e29473ba56249a40
abdullahss <abdullh2004ss@gmail.com>
2024-02-08 23:48:08 +0200

    V1

---
+++ script.js
@@ -0,0 +1,19 @@
+let tablebody = document.getElementById("tableBody")
+
+tablebody.innerHTML=``;
+
+function add(name ,grades , point , hours){
+
+    tablebody.innerHTML += `
+<tr>
+<td>${name}</td>
+<td>${grades}</td>
+<td>${point}</td>
+<td>${hours}</td>
+</tr>
+
+`
+}
+add("ahmed" ,"1" ,"1" ,"1")
+add("ahmed" ,"1" ,"1" ,"1")
+add("ahmed" ,"1" ,"1" ,"1")
